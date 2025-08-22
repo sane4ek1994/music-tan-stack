7560a15d25e06fce41637e2ca032ad78f6d2ebeb
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-08-22 15:44:53 +0300

    added deps openApi, pluginQuery, tanstack/react-query-devtools

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,13 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import {globalIgnores} from 'eslint/config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default tseslint.config([
+    ...pluginQuery.configs['flat/recommended'],
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
