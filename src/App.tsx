2d82e89e74f9db16772cb1211865c665c9b4a507
Alexandr Naumov <mfq07k9cz@gmail.com>
2025-08-25 11:18:56 +0300

    created schema.ts api-gen

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        (async function() {
+            const response = await  fetch('https://musicfun.it-incubator.app/api/1.0/playlists', {
+                headers: {
+                    // TODO удалить этот ключ!
+                    "api-key": '8ca9f37a-81c3-44d3-b627-0a28713999e7'
+                }  })
+
+            const data =  response.json()
+            console.log(data)
+        })()
+    }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Hello</div>
     </>
   )
 }
